fc2e677435795d607060f35c1a5a4f44ca673db7
Elliot Killick <elliotkillick@zohomail.eu>
2019-08-24 08:58:10 +0000

    Add install-qvm-create-windows-qube.sh

---
+++ install-qvm-create-windows-qube.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$(hostname)" != "dom0" ]; then
+    echo "This script must be run in Dom0!" >&2
+    exit 1
+fi
+
+# Create a new AppVM
+qvm-create --class AppVM --template fedora-30 --label black windows-mgmt
+
+# Increase storage capacity
+qvm-volume extend windows-mgmt:private 20480M
+
+# Clone qvm-create-windows-qube repository
+qvm-run -p windows-mgmt 'cd Documents && git clone https://github.com/crazyqube/qvm-create-windows-qube'
+
+# Run the script to download Windows
+qvm-run -p windows-mgmt 'cd Documents/qvm-create-windows-qube && ./download-windows.sh'
+
+# Install Qubes Windows Tools
+sudo qubes-dom0-update --enablerepo=qubes-dom0-current-testing qubes-windows-tools
+
+# Copy script to Dom0
+qvm-run -p windows-mgmt 'cat $HOME/Documents/qvm-create-windows-qube/qvm-create-windows-qube.sh' > qvm-create-windows-qube.sh
