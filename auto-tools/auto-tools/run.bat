dd4af82e1f75f5d1971540aaefb6ef28d0f740fb
Elliot Killick <elliotkillick@zohomail.eu>
2019-09-11 22:06:31 +0000

    Cleanup run.bat

--- auto-tools/auto-tools/run.bat
+++ auto-tools/auto-tools/run.bat
@@ -3,13 +3,12 @@ title %~f0
 
 rem Automatically run by answer file upon first boot after installation
 
-cd /d D:
+cd /d D:\
 
 start cmd /c "cd chocolatey && "get-packages.bat""
-rem Run now because it take one restart for this setting to take effect
+start cmd /c "cd updates && "install-updates.bat""
 start cmd /c "cd qubes-windows-tools && "allow-unsigned-drivers.bat""
 start cmd /c "cd qubes-windows-tools && "install-qwt-startup-task.bat""
-start cmd /c "cd updates && "install-updates.bat""
 
 rem If the PPID of any cmd.exe is equal to the PID of this process we will wait for them to exit and then shutdown
 for /f "tokens=2 usebackq" %%p in (`tasklist /v /fi "IMAGENAME eq cmd.exe" ^| findstr /l %~f0`) do set PID=%%p
