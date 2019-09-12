a61c4319f274d632b728da687d2a9748c87abf80
Elliot Killick <elliotkillick@zohomail.eu>
2019-09-12 00:51:34 +0000

    Increase qrexec_timeout for safety

--- qvm-create-windows-qube.sh
+++ qvm-create-windows-qube.sh
@@ -177,7 +177,7 @@ for (( counter = 1; counter <= count; counter++ )); do
     qvm-prefs "$current_name" memory 400
     qvm-prefs "$current_name" maxmem 0 # Disables currently unstable Qubes memory manager (Also grays the option out in Qubes Manager)
     qvm-prefs "$current_name" kernel ''
-    qvm-prefs "$current_name" qrexec_timeout 120 # Windows startup can take longer, especially if a disk scan is performed
+    qvm-prefs "$current_name" qrexec_timeout 300 # Windows startup can take longer, especially if a disk scan is performed
     qvm-features "$current_name" video-model cirrus
     qvm-volume extend "$current_name":root 30g
     qvm-prefs "$current_name" netvm "$netvm"
