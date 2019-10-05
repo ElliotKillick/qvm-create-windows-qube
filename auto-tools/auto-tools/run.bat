e765de128eb91d5d055901ebd0ebc0abd3922686
Elliot Killick <elliotkillick@zohomail.eu>
2019-10-05 19:56:52 +0000

    Use findstr /c: not /l to not use regex

--- auto-tools/auto-tools/run.bat
+++ auto-tools/auto-tools/run.bat
@@ -11,9 +11,7 @@ start cmd /c "cd qubes-windows-tools && "allow-unsigned-drivers.bat""
 start cmd /c "cd qubes-windows-tools && "install-qwt-startup-task.bat""
 
 rem If the PPID of any cmd.exe is equal to the PID of this process we will wait for them to exit and then shutdown
-for /f "tokens=2 usebackq" %%p in (`tasklist /v /fi "IMAGENAME eq cmd.exe" ^| findstr /l %~f0`) do set PID=%%p
+for /f "tokens=2 usebackq" %%p in (`tasklist /v /fi "IMAGENAME eq cmd.exe" ^| findstr /c:"%~f0"`) do set PID=%%p
 :jobs_running
-wmic process where (parentprocessid="%PID%" and name="cmd.exe") | findstr /l "No Instance(s) Available." >nul
-if %ERRORLEVEL%==0 timeout 5 /nobreak && goto jobs_running
-
-shutdown /s /t 0
+wmic process where (parentprocessid="%PID%" and name="cmd.exe") 2>&1 | findstr /c:"No Instance(s) Available." >nul
+if %ERRORLEVEL%==0 (shutdown /s /t 0) else timeout 5 /nobreak && goto jobs_running
