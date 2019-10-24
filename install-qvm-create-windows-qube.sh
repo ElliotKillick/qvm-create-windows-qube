22678cc76064b565004114c1e09fa6748c14246d
Elliot Killick <elliotkillick@zohomail.eu>
2019-10-24 14:04:06 +0000

    Use qube terminology for variable instead of vm

--- install-qvm-create-windows-qube.sh
+++ install-qvm-create-windows-qube.sh
@@ -7,26 +7,26 @@ if [ "$(hostname)" != "dom0" ]; then
     exit 1
 fi
 
-resources_vm="windows-mgmt"
+resources_qube="windows-mgmt"
 resources_dir="/home/user/Documents/qvm-create-windows-qube"
 
 # Create a new AppVM
-qvm-create --class AppVM --template fedora-30 --label black "$resources_vm"
+qvm-create --class AppVM --template fedora-30 --label black "$resources_qube"
 
 # Increase storage capacity
-qvm-volume extend "$resources_vm":private 20480M
+qvm-volume extend "$resources_qube":private 20480M
 
 # Clone qvm-create-windows-qube repository
-qvm-run -p "$resources_vm" "cd /home/user/Documents && git clone https://github.com/crazyqube/qvm-create-windows-qube"
+qvm-run -p "$resources_qube" "cd /home/user/Documents && git clone https://github.com/crazyqube/qvm-create-windows-qube"
 
 # Run the script to download Windows
-qvm-run -p "$resources_vm" "cd '$resources_dir' && ./download-windows.sh"
+qvm-run -p "$resources_qube" "cd '$resources_dir' && ./download-windows.sh"
 
 # Install Qubes Windows Tools
 sudo qubes-dom0-update --enablerepo=qubes-dom0-current-testing qubes-windows-tools
 
 # Copy script into Dom0
-qvm-run -p "$resources_vm" "cat '$resources_dir/qvm-create-windows-qube.sh' > qvm-create-windows-qube.sh"
+qvm-run -p "$resources_qube" "cat '$resources_dir/qvm-create-windows-qube.sh' > qvm-create-windows-qube.sh"
 
 # Allow execution of script
 chmod +x qvm-create-windows-qube.sh
