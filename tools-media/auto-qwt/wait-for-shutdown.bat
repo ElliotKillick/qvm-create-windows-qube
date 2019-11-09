cebe09849c3ff4b7ba1a40728eb3dafad1040352
Elliot Killick <elliotkillick@zohomail.eu>
2019-11-09 21:01:09 +0000

    Always do goto after timeout and change formatting

--- tools-media/auto-qwt/wait-for-shutdown.bat
+++ tools-media/auto-qwt/wait-for-shutdown.bat
@@ -5,4 +5,4 @@ rem Necessary to close "Qubes private disk image initalized as disk <drive>" pro
 set app=prepare-volume.exe
 :qwt_installing
 tasklist /fi "IMAGENAME eq %app%" /fi "WINDOWTITLE eq Qubes Tools for Windows" | find "%app%" >nul
-if %ERRORLEVEL%==0 (taskkill /im "%app%") else timeout 1 /nobreak && goto qwt_installing
+if %ERRORLEVEL%==0 (taskkill /im "%app%") else (timeout 1 /nobreak & goto qwt_installing)
