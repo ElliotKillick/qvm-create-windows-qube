d3b95ca0f3cca2d452171bbe90320058ff097336
Elliot Killick <elliotkillick@zohomail.eu>
2019-11-09 06:23:08 +0000

    Use find instead of regex version findstr

--- tools-media/auto-qwt/wait-for-shutdown.bat
+++ tools-media/auto-qwt/wait-for-shutdown.bat
@@ -4,5 +4,5 @@ title %~f0
 rem Necessary to close "Qubes private disk image initalized as disk <drive>" prompt so installer will proceed to reboot computer
 set app=prepare-volume.exe
 :qwt_installing
-tasklist /fi "IMAGENAME eq %app%" /fi "WINDOWTITLE eq Qubes Tools for Windows" | findstr /c:"%app%" >nul
+tasklist /fi "IMAGENAME eq %app%" /fi "WINDOWTITLE eq Qubes Tools for Windows" | find "%app%" >nul
 if %ERRORLEVEL%==0 (taskkill /im "%app%") else timeout 1 /nobreak && goto qwt_installing
