467c01d855f393ab19e235f1540d9a07c3728536
Elliot Killick <elliotkillick@zohomail.eu>
2019-11-21 05:04:57 +0000

    Put private image on D: instead of E:

--- tools-media/auto-qwt/run.bat
+++ tools-media/auto-qwt/run.bat
@@ -3,8 +3,14 @@ title %~f0
 
 rem Automatically run by answer file upon first boot after installation
 
-rem Change directory to location of this batch file
-cd /d "%~dp0"
+set from_drive=D:
+set to_drive=E:
+if "%~d0" == "%from_drive%" (
+    rem After changing the drive letter the batch file is on, the command processor will no longer be able to read from it and close
+    rem Therefore, we must run run.bat again
+    start /wait cmd /c "%from_drive%\change-drive-letter.bat %to_drive%" & start cmd /c "%to_drive%\run.bat"
+)
+cd /d %to_drive%
 
 start cscript "allow-device-software.vbs"
 start cmd /c "install-qwt.bat"
