948ed9dfed8b75b0a8b70c51d0927cfabb0d2529
Elliot Killick <elliotkillick@zohomail.eu>
2019-12-24 08:25:54 +0000

    Add comment about Windows 10 telemetry

--- post/anti-spy.bat
+++ post/anti-spy.bat
@@ -3,4 +3,7 @@ title %~f0
 
 rem Answer file disables Customer Experience Improvement Program (CEIP) and Windows Error Reporting (WER)
 
+rem In Windows 10, ProtectYourPC disables more telemetry (It functions differently from previous Windows versions where it configured automatic updates)
+rem https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-oobe-protectyourpc
+
 sc config DiagTrack start= disabled
