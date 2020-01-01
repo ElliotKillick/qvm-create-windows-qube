e5daa4dcb65e87b1197187485606cc9ec6570f11
Elliot Killick <elliotkillick@zohomail.eu>
2020-01-01 05:34:09 +0000

    Comment on why DiagTrack is being disabled

--- post/anti-spy.bat
+++ post/anti-spy.bat
@@ -6,4 +6,6 @@ rem Answer file disables Customer Experience Improvement Program (CEIP) and Wind
 rem In Windows 10, ProtectYourPC disables more telemetry (It functions differently from previous Windows versions where it configured automatic updates)
 rem https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-oobe-protectyourpc
 
+rem The DiagTrack service is used for sending telemetry data to Microsoft
+rem This opts out of even "Basic" telemetry
 sc config DiagTrack start= disabled
