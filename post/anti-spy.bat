5104e60a05a2d195c0067b18a210939d2d805d14
Elliot Killick <elliotkillick@zohomail.eu>
2020-01-04 15:04:06 +0000

    Add comment on how to disable more telemetry

--- post/anti-spy.bat
+++ post/anti-spy.bat
@@ -12,6 +12,10 @@ rem Only works on Windows 10 Enterprise, Education, Mobile Enterprise, and IoT C
 rem Still doesn't disable everything: https://www.softscheck.com/en/privacy-analysis-windows-10-enterprise-telemetry-level-0/
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v AllowTelemetry /t REG_DWORD /d 0 /f
 
-rem The DiagTrack service is used for sending telemetry data to Microsoft
-rem This disables sending all telemetry data
+rem The DiagTrack service is used for sending Windows telemetry data to Microsoft
+rem This disables sending all Windows telemetry data
 sc config DiagTrack start= disabled
+
+rem This disables the bulk of the telemetry, at least for what is necessary in a VM, while providing zero impact on user experience
+rem If you're looking to go further I recommend this script which is based on official Microsoft documentation (See .NOTES section): https://github.com/cryps1s/DARKSURGEON/blob/master/configuration/configuration-scripts/Set-WindowsTelemetrySettings.ps1
+rem Of course, the best option is to air gap Windows or refrain from using it
