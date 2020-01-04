2a97e0fe4e83680a898a3362b22488278708db9e
Elliot Killick <elliotkillick@zohomail.eu>
2020-01-04 08:51:21 +0000

    Simplify comment

--- tools-media/auto-qwt/wait-for-shutdown.bat
+++ tools-media/auto-qwt/wait-for-shutdown.bat
@@ -1,7 +1,7 @@
 @echo off
 title %~f0
 
-rem Necessary to close "Qubes private disk image initalized as disk <drive>" prompt so installer will proceed to reboot computer
+rem Closes "Qubes private disk image initalized as disk <drive>" prompt so installer will proceed to reboot computer
 set app=prepare-volume.exe
 :qwt_installing
 tasklist /fi "IMAGENAME eq %app%" /fi "WINDOWTITLE eq Qubes Tools for Windows" | find "%app%" >nul
