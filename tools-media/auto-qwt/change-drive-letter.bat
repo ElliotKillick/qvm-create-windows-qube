02294f2ec7b6eeba3c1d98beca90ff6e7806b6db
Elliot Killick <elliotkillick@zohomail.eu>
2020-01-04 10:13:33 +0000

    Improve comment accuracy

--- tools-media/auto-qwt/change-drive-letter.bat
+++ tools-media/auto-qwt/change-drive-letter.bat
@@ -1,7 +1,7 @@
 @echo off
 title %~f0
 
-rem Change drive letter of auto-qwt from D: to E: so QWT can install the private image on D: (Purely cosmetic)
+rem Change drive letter of auto-qwt from D: to E: so QWT can install the private volume on D: (Purely cosmetic)
 
 (echo select volume 0
 echo assign letter=%1
