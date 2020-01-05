341fed477797a4a0d947e5fad61626c5bf222991
Elliot Killick <elliotkillick@zohomail.eu>
2020-01-05 13:53:13 +0000

    Don't disable network prompt
    This setting is now purely cosmetic and its effects are persistent
    across reboots. We should try not to modify the system unnecessarily.
    Also, I noticed VMWare did not disable the network prompt in their
    implementation of this project idea.

--- tools-media/auto-qwt/disable-network-prompt.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title %~f0
-
-rem Disable network prompt because it's an unnecessary prompt that won't be interacted with (Purely cosmetic)
-
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowOff" /f
--- tools-media/auto-qwt/run.bat
+++ tools-media/auto-qwt/run.bat
@@ -7,9 +7,6 @@ set from_drive=D:
 set to_drive=E:
 
 if "%~d0" == "%from_drive%" (
-    rem Changing the driver letter takes a while so we must disable the network prompt now before it is displayed
-    start /wait cmd /c "%from_drive%\disable-network-prompt.bat"
-
     rem After changing the drive letter the batch file is on, the command processor will no longer be able to read from it and close
     rem Therefore, we must run run.bat again
     start /wait cmd /c "%from_drive%\change-drive-letter.bat %to_drive%" & start cmd /c "%to_drive%\run.bat"
