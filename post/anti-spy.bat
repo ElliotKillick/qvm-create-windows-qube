34c9605e8214a816358eee568b0c19f929a8f674
Elliot Killick <elliotkillick@zohomail.eu>
2020-01-24 12:56:32 +0000

    Improve comments

--- post/anti-spy.bat
+++ post/anti-spy.bat
@@ -3,17 +3,17 @@ title %~f0
 
 rem Answer file disables Customer Experience Improvement Program (CEIP) and Windows Error Reporting (WER)
 
-rem In Windows 10, ProtectYourPC disables more telemetry (It functions differently from previous Windows versions where it configured automatic updates)
+rem In Windows 10 answer files, ProtectYourPC flips switches in the Settings application to disable telemetry in Windows (It functions differently from previous Windows versions where it configured automatic updates)
 rem https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-oobe-protectyourpc
 
 rem Set telemetry level to "Security" which is the lowest possible
 rem Levels of telemetry: https://docs.microsoft.com/en-us/windows/privacy/configure-windows-diagnostic-data-in-your-organization
-rem Only works on Windows 10 Enterprise, Education, Mobile Enterprise, and IoT Core editions, and Windows Server 2016 or higher
-rem Still doesn't disable everything: https://www.softscheck.com/en/privacy-analysis-windows-10-enterprise-telemetry-level-0/
+rem Doesn't work on Windows 10 Pro or lesser editions of Windows 10 made for typical end users (Must be some type of Enterprise edition of Windows 10 or Windows Server 2016+)
+rem Still doesn't disable everything: https://www.softscheck.com/en/privacy-analysis-windows-10-enterprise-telemetry-level-0/ (Does disabling DiagTrack service mitigate this?)
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v AllowTelemetry /t REG_DWORD /d 0 /f
 
 rem The DiagTrack service is used for sending Windows telemetry data to Microsoft
-rem This disables sending all Windows telemetry data
+rem This disables sending of all Windows telemetry data to Microsoft
 sc config DiagTrack start= disabled
 
 rem This disables the bulk of the telemetry, at least for what is necessary in a VM, while providing zero impact on user experience
