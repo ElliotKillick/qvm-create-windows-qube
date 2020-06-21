3f6768e7cc42f513f613f22b7ae540f81cb8bc09
Elliot Killick <elliotkillick@zohomail.eu>
2020-06-21 12:24:30 +0000

    Add further methods for decreasing telemetry

--- post/spyless.bat
+++ post/spyless.bat
@@ -5,7 +5,7 @@ rem Answer file disables Customer Experience Improvement Program (CEIP) and Wind
 reg add "HKLM\SOFTWARE\Microsoft\SQMClient\Windows" /v CEIPEnable /t REG_DWORD /d 0 /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\Windows Error Reporting" /v Disabled /t REG_DWORD /d 1 /f
 
-rem In Windows 10 answer files, ProtectYourPC changes settings in the Settings application to disable telemetry in Windows
+rem In Windows 10 answer files, ProtectYourPC switches off telemetry in the Settings application
 rem This is opposed to its function in previous versions of Windows where it configured automatic updates
 rem https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-oobe-protectyourpc
 
@@ -21,10 +21,23 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v AllowTeleme
 rem The DiagTrack service is used for sending Windows telemetry data to Microsoft
 sc config DiagTrack start= disabled
 
-rem This disables the bulk of the telemetry, at least for what is necessary in a VM, while providing zero impact on user experience
-rem For Windows 7, I think this is sufficient, but for Windows 10 I will recommend the following if you're looking to go further
+rem This disables the bulk of the telemetry, at least for what is necessary in a VM, while providing zero impact on the user experience
+rem Here are things I recommend if you're looking to go further
 
 rem I recommend this script because it's based on official Microsoft documentation (See .NOTES section):
 rem https://github.com/cryps1s/DARKSURGEON/blob/master/configuration/configuration-scripts/Set-WindowsTelemetrySettings.ps1
+
+rem A firewall method may be suitable, however, it may block a few things it shouldn't:
+rem https://gist.github.com/elliotkillick/5762f91960454720bd42193f66d6e0ae
+
+rem Use the LTSC edition of Windows 10 because it's debloated by default
+rem This means it's not necessary to run Microsoft unapproved tools to debloat Windows
+
+rem With all these methods combined, Windows telemetry is greatly diminished
+rem This can be seen in Wireshark by the fact that Windows is not reaching out to Microsoft servers nearly as often as it did originally
+rem This is at least once you've had Windows online for a while because it connects out a lot when it's first installed
+
+rem Be aware that while this does a good job at disabling telemetry of Windows itself, this doesn't account for other Microsoft (e.g. Office 365) or third-party products
+rem More research is required here
 
 rem Of course, the best option is to air gap Windows or refrain from using it
