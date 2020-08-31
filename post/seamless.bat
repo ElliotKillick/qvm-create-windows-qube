59ce0b09885d629344ea2f1569363c0c7270abcc
Elliot Killick <elliotkillick@zohomail.eu>
2020-08-31 22:24:16 +0000

    Make post script desc. consistent with README

--- post/seamless.bat
+++ post/seamless.bat
@@ -1,6 +1,6 @@
 @echo off
 title %~f0
 
-rem Enable seamless mode persistently across boots
+rem Enable seamless mode persistently across reboots
 
 reg add "HKLM\SOFTWARE\Invisible Things Lab\Qubes Tools\qga" /v SeamlessMode /t REG_DWORD /d 1 /f
