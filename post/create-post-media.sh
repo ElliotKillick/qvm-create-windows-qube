4fe6f694b5c3efb797da46b6415d0027e9df17a2
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2020-12-14 22:17:33 +0100

    post: set current content as default reference

--- post/create-post-media.sh
+++ post/create-post-media.sh
@@ -7,7 +7,7 @@ scriptsdir="$(readlink -f "$localdir/../scripts")"
 # shellcheck source=scripts/common.sh
 source "$scriptsdir/common.sh"
 
-content="${1:-"$localdir/content"}"
+content="${1:-"$localdir/default"}"
 
 echo_info "Preparing post media from '$content'..."
 genisoimage -input-charset utf-8 -JR -o "$localdir/post.iso" "$content"
