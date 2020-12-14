d72251dacf87120f3fcaa642f93248ddae90e5a2
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2020-12-14 22:16:43 +0100

    post: allow to specify content folder

--- post/create-post-media.sh
+++ post/create-post-media.sh
@@ -7,5 +7,7 @@ scriptsdir="$(readlink -f "$localdir/../scripts")"
 # shellcheck source=scripts/common.sh
 source "$scriptsdir/common.sh"
 
-echo_info "Preparing default post ISO..."
-genisoimage -input-charset utf-8 -JR -o "$localdir/post.iso" "content"
+content="${1:-"$localdir/content"}"
+
+echo_info "Preparing post media from '$content'..."
+genisoimage -input-charset utf-8 -JR -o "$localdir/post.iso" "$content"
