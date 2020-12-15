f5a0c88b37e3bb6f001b3308b2e0d16791058e9a
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2020-12-15 11:06:56 +0100

    download-windows: refactor

--- windows/isos/download-windows.sh
+++ windows/isos/download-windows.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Test for 4-bit color (16 colors)
-if [ "0$(tput colors 2> /dev/null)" -ge 16 ]; then
-    RED='\033[0;31m'
-    BLUE='\033[0;34m'
-    GREEN='\033[0;32m'
-    NC='\033[0m'
-fi
+[[ "$DEBUG" == 1 ]] && set -x
+localdir="$(readlink -f "$(dirname "$0")")"
+scriptsdir="$(readlink -f "$localdir/../scripts")"
+
+# shellcheck source=scripts/common.sh
+source "$scriptsdir/common.sh"
 
 usage() {
     echo "Usage: $0 <windows_media>..."
@@ -76,7 +75,7 @@ for arg in "$@"; do
             all="true"
             ;;
         *)
-            echo -e "${RED}[!]${NC} Invalid Windows media specified: $arg" >&2
+            echo_err "Invalid Windows media specified: $arg" >&2
             exit 1
             ;;
     esac
@@ -94,82 +93,82 @@ scurl_file() {
     url="$4"
 
     until curl --output "$out_file" --pinnedpubkey "$pinned_public_key" --"tlsv$tls_version" --proto =https "$url"; do
-        echo -e "${RED}[!]${NC} Failed to download Windows! Retrying in 10 seconds..." >&2
+        echo_err "Failed to download Windows! Retrying in 10 seconds..." >&2
         sleep 10
     done
 }
 
-echo -e "${BLUE}[i]${NC} Downloading Windows media from Microsoft servers..." >&2
+echo_info "Downloading Windows media from Microsoft servers..." >&2
 
 if [ "$win7x64_ultimate" ] || [ "$all" ]; then
-    echo -e "${BLUE}[i]${NC} Downloading Windows 7..." >&2
+    echo_info "Downloading Windows 7..." >&2
     # Heidoc Windows ISO Downloader
-    scurl_file "win7x64-ultimate.iso" "$download_microsoft_com_key" "1.2" "https://download.microsoft.com/download/5/1/9/5195A765-3A41-4A72-87D8-200D897CBE21/7601.24214.180801-1700.win7sp1_ldr_escrow_CLIENT_ULTIMATE_x64FRE_en-us.iso"
+    scurl_file "$localdir/isos/win7x64-ultimate.iso" "$download_microsoft_com_key" "1.2" "https://download.microsoft.com/download/5/1/9/5195A765-3A41-4A72-87D8-200D897CBE21/7601.24214.180801-1700.win7sp1_ldr_escrow_CLIENT_ULTIMATE_x64FRE_en-us.iso"
 fi
 
 if [ "$win81x64" ] || [ "$all" ]; then
-    echo -e "${BLUE}[i]${NC} Downloading Windows 8.1..." >&2
+    echo_info "Downloading Windows 8.1..." >&2
     # Doesn't work because Microsoft gives links that are only valid for 24 hours (https://serverfault.com/questions/952196/how-do-i-download-a-win10x64-enterprise-eval-iso-file-from-microsoft-via-the-command-line)
     # https://www.microsoft.com/en-us/software-download/windows8ISO
     # Returns 403 forbidden: curl "https://software-download.microsoft.com/pr/Win8.1_English_x64.iso" -o "win81x64.iso
-    echo -e "${RED}[!]${NC} Microsoft does not allow automatic downloading of this Windows media so please download it manually here: https://www.microsoft.com/en-us/software-download/windows8ISO (Don't forget to verify it afterwards)" >&2
+    echo_err "Microsoft does not allow automatic downloading of this Windows media so please download it manually here: https://www.microsoft.com/en-us/software-download/windows8ISO (Don't forget to verify it afterwards)" >&2
 fi
 
 if [ "$win10x64" ] || [ "$all" ]; then
-    echo -e "${BLUE}[i]${NC} Downloading Windows 10..." >&2
+    echo_info "Downloading Windows 10..." >&2
     # Doesn't work for same reason as Windows 8.1
     # https://www.microsoft.com/en-us/software-download/windows10ISO
     # Returns 403 forbidden: curl "https://software-download.microsoft.com/sg/Win10_1909_English_x64.iso" -o "win10x64.iso"
-    echo -e "${RED}[!]${NC} Microsoft does not allow automatic downloading of this Windows media so please download it manually here: https://www.microsoft.com/en-us/software-download/windows10ISO (Don't forget to verify it afterwards)" >&2
+    echo_err "Microsoft does not allow automatic downloading of this Windows media so please download it manually here: https://www.microsoft.com/en-us/software-download/windows10ISO (Don't forget to verify it afterwards)" >&2
 fi
 
 if [ "$win81x64_enterprise_eval" ] || [ "$all" ]; then
-    echo -e "${BLUE}[i]${NC} Downloading Windows 8.1 Enterprise Evaluation..." >&2
+    echo_info "Downloading Windows 8.1 Enterprise Evaluation..." >&2
     # Used to be here but got taken down: https://www.microsoft.com/en-us/evalcenter/evaluate-windows-8-1-enterprise
     # https://gist.github.com/eyecatchup/11527136b23039a0066f
-    scurl_file "win81x64-enterprise-eval.iso" "$download_microsoft_com_key" "1.2" "https://download.microsoft.com/download/B/9/9/B999286E-0A47-406D-8B3D-5B5AD7373A4A/9600.17050.WINBLUE_REFRESH.140317-1640_X64FRE_ENTERPRISE_EVAL_EN-US-IR3_CENA_X64FREE_EN-US_DV9.ISO"
+    scurl_file "$localdir/isos/win81x64-enterprise-eval.iso" "$download_microsoft_com_key" "1.2" "https://download.microsoft.com/download/B/9/9/B999286E-0A47-406D-8B3D-5B5AD7373A4A/9600.17050.WINBLUE_REFRESH.140317-1640_X64FRE_ENTERPRISE_EVAL_EN-US-IR3_CENA_X64FREE_EN-US_DV9.ISO"
 fi
 
 if [ "$win10x64_enterprise_eval" ] || [ "$all" ]; then
-    echo -e "${BLUE}[i]${NC} Downloading Windows 10 Enterprise Evaluation..." >&2
+    echo_info "Downloading Windows 10 Enterprise Evaluation..." >&2
     # https://www.microsoft.com/en-us/evalcenter/evaluate-windows-10-enterprise
-    scurl_file "win10x64-enterprise-eval.iso" "$software_download_microsoft_com_key" "1.3" "https://software-download.microsoft.com/download/pr/18363.418.191007-0143.19h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso"
+    scurl_file "$localdir/isos/win10x64-enterprise-eval.iso" "$software_download_microsoft_com_key" "1.3" "https://software-download.microsoft.com/download/pr/18363.418.191007-0143.19h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso"
 fi
 
 if [ "$win10x64_ltsc_eval" ] || [ "$all" ]; then
-    echo -e "${BLUE}[i]${NC} Downloading Windows 10 Enterprise LTSC Evaluation..." >&2
+    echo_info "Downloading Windows 10 Enterprise LTSC Evaluation..." >&2
     # https://www.microsoft.com/en-us/evalcenter/evaluate-windows-10-enterprise
-    scurl_file "win10x64-ltsc-eval.iso" "$software_download_microsoft_com_key" "1.3" "https://software-download.microsoft.com/download/sg/17763.107.101029-1455.rs5_release_svc_refresh_CLIENT_LTSC_EVAL_x64FRE_en-us.iso"
+    scurl_file "$localdir/isos/win10x64-ltsc-eval.iso" "$software_download_microsoft_com_key" "1.3" "https://software-download.microsoft.com/download/sg/17763.107.101029-1455.rs5_release_svc_refresh_CLIENT_LTSC_EVAL_x64FRE_en-us.iso"
 fi
 
 if [ "$win2008r2" ] || [ "$all" ]; then
-    echo -e "${BLUE}[i]${NC} Downloading Windows Server 2008 R2..." >&2
+    echo_info "Downloading Windows Server 2008 R2..." >&2
     # https://www.microsoft.com/en-us/download/details.aspx?id=11093
-    scurl_file "win2008r2.iso" "$download_microsoft_com_key" "1.2" "https://download.microsoft.com/download/7/5/E/75EC4E54-5B02-42D6-8879-D8D3A25FBEF7/7601.17514.101119-1850_x64fre_server_eval_en-us-GRMSXEVAL_EN_DVD.iso"
+    scurl_file "$localdir/isos/win2008r2.iso" "$download_microsoft_com_key" "1.2" "https://download.microsoft.com/download/7/5/E/75EC4E54-5B02-42D6-8879-D8D3A25FBEF7/7601.17514.101119-1850_x64fre_server_eval_en-us-GRMSXEVAL_EN_DVD.iso"
 fi
 
 if [ "$win2012r2_eval" ] || [ "$all" ]; then
-    echo -e "${BLUE}[i]${NC} Downloading Windows Server 2012 R2..." >&2
+    echo_info "Downloading Windows Server 2012 R2..." >&2
     # https://www.microsoft.com/en-us/evalcenter/evaluate-windows-server-2012-r2
-    scurl_file "win2012r2-eval.iso" "$download_microsoft_com_key" "1.2" "https://download.microsoft.com/download/6/2/A/62A76ABB-9990-4EFC-A4FE-C7D698DAEB96/9600.17050.WINBLUE_REFRESH.140317-1640_X64FRE_SERVER_EVAL_EN-US-IR3_SSS_X64FREE_EN-US_DV9.ISO"
+    scurl_file "$localdir/isos/win2012r2-eval.iso" "$download_microsoft_com_key" "1.2" "https://download.microsoft.com/download/6/2/A/62A76ABB-9990-4EFC-A4FE-C7D698DAEB96/9600.17050.WINBLUE_REFRESH.140317-1640_X64FRE_SERVER_EVAL_EN-US-IR3_SSS_X64FREE_EN-US_DV9.ISO"
 fi
 
 if [ "$win2016_eval" ] || [ "$all" ]; then
-    echo -e "${BLUE}[i]${NC} Downloading Windows Server 2016..." >&2
+    echo_info "Downloading Windows Server 2016..." >&2
     # https://www.microsoft.com/en-us/evalcenter/evaluate-windows-server-2016
-    scurl_file "win2016-eval.iso" "$software_download_microsoft_com_key" "1.3" "https://software-download.microsoft.com/download/pr/win2016_eval_Datacenter_EVAL_en-us_14393_refresh.ISO"
+    scurl_file "$localdir/isos/win2016-eval.iso" "$software_download_microsoft_com_key" "1.3" "https://software-download.microsoft.com/download/pr/win2016_eval_Datacenter_EVAL_en-us_14393_refresh.ISO"
 fi
 
 if [ "$win2019_eval" ] || [ "$all" ]; then
-    echo -e "${BLUE}[i]${NC} Downloading Windows Server 2019..." >&2
+    echo_info "Downloading Windows Server 2019..." >&2
     # https://www.microsoft.com/en-us/evalcenter/evaluate-windows-server-2019
-    scurl_file "win2019-eval.iso" "$software_download_microsoft_com_key" "1.3" "https://software-download.microsoft.com/download/pr/17763.737.190906-2324.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us_1.iso"
+    scurl_file "$localdir/isos/win2019-eval.iso" "$software_download_microsoft_com_key" "1.3" "https://software-download.microsoft.com/download/pr/17763.737.190906-2324.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us_1.iso"
 fi
 
-echo -e "${BLUE}[i]${NC} Verifying integrity..." >&2
+echo_info "Verifying integrity..." >&2
 if ! sha256sum -c SHA256SUMS --ignore-missing; then
-    echo -e "${RED}[!]${NC} One or more of the downloaded Windows media did not match the expected hash! This means either the media was corrupted during download or that is has been (potentially maliciously) modified! Please re-attempt the download and do not use the bad media." >&2
+    echo_err "One or more of the downloaded Windows media did not match the expected hash! This means either the media was corrupted during download or that is has been (potentially maliciously) modified! Please re-attempt the download and do not use the bad media." >&2
     exit 1
 fi
 
-echo -e "${GREEN}[+]${NC} Successfully downloaded and verified integrity of Windows media!"
+echo_done "Successfully downloaded and verified integrity of Windows media!"
