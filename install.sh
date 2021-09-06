d2dba0b6065cbae6eb419aa8ae0aeacc474d7581
Elliot Killick <elliotkillick@zohomail.eu>
2021-09-06 04:44:40 -0400

    Import PGP key locally

--- install.sh
+++ install.sh
@@ -97,8 +97,7 @@ echo -e "${BLUE}[i]${NC} Cloning qvm-create-windows-qube GitHub repository..." >
 qvm-run -p "$resources_qube" "cd ${resources_dir%/*} && git clone https://github.com/elliotkillick/qvm-create-windows-qube"
 
 echo -e "${BLUE}[i]${NC} Please check for a \"Good signature\" from GPG (Verify it out-of-band if necessary)..." >&2
-qvm-run -q "$resources_qube" "gpg --keyserver keys.openpgp.org --recv-keys 018FB9DE6DFA13FB18FB5552F9B90D44F83DD5F2"
-qvm-run -p "$resources_qube" "cd '$resources_dir' && git verify-commit \$(git rev-list --max-parents=0 HEAD)"
+qvm-run -p "$resources_qube" "cd '$resources_dir' && gpg --import author.asc && git verify-commit \$(git rev-list --max-parents=0 HEAD)"
 
 echo -e "${BLUE}[i]${NC} Remember to download the desired version(s) of Windows using download-windows.sh in the $resources_qube qube..." >&2
 qvm-run -p "$resources_qube" "'$resources_dir/windows-media/isos/download-windows.sh' --help"
