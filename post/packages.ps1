e0e369c25f892af470b5b98f989cab97864fb25b
Elliot Killick <elliotkillick@zohomail.eu>
2023-06-13 08:03:27 -0400

    packages.ps1: Feature test before forcing TLS 1.2
    This way we get TLS 1.3 in case it's supported. Also, print a message
    about what's happening and to ignore Chocolatey's incorrect assertions.

--- post/packages.ps1
+++ post/packages.ps1
@@ -15,7 +15,10 @@ Param (
 $host.UI.RawUI.WindowTitle = $PSCommandPath
 
 # Force Powershell 2 to use TLS 1.2
-[System.Net.ServicePointManager]::SecurityProtocol = [Enum]::ToObject([System.Net.SecurityProtocolType], 3072)
+if ([System.Net.SecurityProtocolType]::Tls12 -eq $null) {
+    Write-Host "Enabling TLS 1.2 for PowerShell 2 (please ignore Chocolatey if it claims that TLS 1.0 is in use)..."
+    [System.Net.ServicePointManager]::SecurityProtocol = [Enum]::ToObject([System.Net.SecurityProtocolType], 3072)
+}
 
 # https://chocolatey.org/install
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
